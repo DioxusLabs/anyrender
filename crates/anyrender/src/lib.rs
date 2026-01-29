33ec3903690c269fa6e28533a9721ae61f13cd3e
Nico Burns <nico@nicoburns.com>
2026-01-29 16:19:29 +0000

    Add append_scene method to PaintScene

--- crates/anyrender/src/lib.rs
+++ crates/anyrender/src/lib.rs
@@ -28,7 +28,8 @@
 //!  - [anyrender_vello_cpu](https://docs.rs/anyrender_vello_cpu)
 
 use kurbo::{Affine, Rect, Shape, Stroke};
-use peniko::{BlendMode, Color, Fill, FontData, ImageBrushRef, StyleRef};
+use peniko::{BlendMode, Brush, Color, Fill, FontData, ImageBrushRef, StyleRef};
+use recording::RenderCommand;
 use std::sync::Arc;
 
 pub mod wasm_send_sync;
@@ -152,6 +153,64 @@ pub trait PaintScene {
     );
 
     // --- Provided methods
+
+    /// Append a recorded Scene Fragment to the current scene
+    fn append_scene(&mut self, scene: Scene) {
+        for cmd in scene.commands {
+            match cmd {
+                RenderCommand::PushLayer(cmd) => {
+                    self.push_layer(cmd.blend, cmd.alpha, cmd.transform, &cmd.clip)
+                }
+                RenderCommand::PushClipLayer(cmd) => self.push_clip_layer(cmd.transform, &cmd.clip),
+                RenderCommand::PopLayer => self.pop_layer(),
+                RenderCommand::Stroke(cmd) => self.stroke(
+                    &cmd.style,
+                    cmd.transform,
+                    match cmd.brush {
+                        Brush::Solid(alpha_color) => Brush::Solid(alpha_color),
+                        Brush::Gradient(ref gradient) => Brush::Gradient(gradient),
+                        Brush::Image(ref image) => Brush::Image(image.as_ref()),
+                    },
+                    cmd.brush_transform,
+                    &cmd.shape,
+                ),
+                RenderCommand::Fill(cmd) => self.fill(
+                    cmd.fill,
+                    cmd.transform,
+                    match cmd.brush {
+                        Brush::Solid(alpha_color) => Brush::Solid(alpha_color),
+                        Brush::Gradient(ref gradient) => Brush::Gradient(gradient),
+                        Brush::Image(ref image) => Brush::Image(image.as_ref()),
+                    },
+                    cmd.brush_transform,
+                    &cmd.shape,
+                ),
+                RenderCommand::GlyphRun(cmd) => self.draw_glyphs(
+                    &cmd.font_data,
+                    cmd.font_size,
+                    cmd.hint,
+                    &cmd.normalized_coords,
+                    &cmd.style,
+                    match cmd.brush {
+                        Brush::Solid(alpha_color) => Brush::Solid(alpha_color),
+                        Brush::Gradient(ref gradient) => Brush::Gradient(gradient),
+                        Brush::Image(ref image) => Brush::Image(image.as_ref()),
+                    },
+                    cmd.brush_alpha,
+                    cmd.transform,
+                    cmd.glyph_transform,
+                    cmd.glyphs.into_iter(),
+                ),
+                RenderCommand::BoxShadow(cmd) => self.draw_box_shadow(
+                    cmd.transform,
+                    cmd.rect,
+                    cmd.brush,
+                    cmd.radius,
+                    cmd.std_dev,
+                ),
+            }
+        }
+    }
 
     /// Utility method to draw an image at it's natural size. For more advanced image drawing use the `fill` method
     fn draw_image(&mut self, image: ImageBrushRef, transform: Affine) {
